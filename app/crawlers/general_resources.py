8b313d27172f898366403e0b424f00c165d00199
unknown <cristian.walteros@coduct.com>
2023-02-13 16:23:17 +0100

    add update container registry.yml

--- app/crawlers/general_resources.py
+++ app/crawlers/general_resources.py
@@ -5,30 +5,14 @@ from selenium.webdriver.firefox.options import Options
 import sys
 sys.path.append('/app/utilities')
 from logger import mylogger
+import os
+SELENIUM_HUB = os.environ.get('SELENIUM_HUB')
 
 class seleniumConection:
     def __init__(self):
-        # mylogger.info(options)
-        # options = Options() 
-        # options.to_capabilities().timeouts
-        self._browser = webdriver.Remote(command_executor='http://selenium:4444/wd/hub',
+        remote_url = SELENIUM_HUB + '/wd/hub'
+        self._browser = webdriver.Remote(command_executor=remote_url,
             desired_capabilities=DesiredCapabilities.FIREFOX)
-
-        # self._browser = webdriver.Remote(command_executor='https://coduct-crawler-selenium.braveforest-44e154a2.westeurope.azurecontainerapps.io/wd/hub',
-        # desired_capabilities=DesiredCapabilities.FIREFOX)
-        # self._browser = webdriver.Remote(command_executor='https://coduct-crawler-selenium.braveforest-44e154a2.westeurope.azurecontainerapps.io/wd/hub',
-        #   desired_capabilities=DesiredCapabilities.FIREFOX)
-        # self._browser = webdriver.Remote(command_executor='https://selenium-app-web.azurewebsites.net//wd/hub',
-        #     desired_capabilities=DesiredCapabilities.FIREFOX)
-        # driver2 = webdriver.Remote(command_executor=the_known_url)  
-        # self._browser.caps.timeouts.pageLoad
-        # self._browser.set_page_load_timeout(60)
-        # mylogger.info(f"timeouts - {self._browser.caps.timeouts}")
-        
-        # mylogger.info(f"browser - {self._browser.__dict__}")
-        # mylogger.info(f"options - {options.__dict__}")
-        # self._browser.SET_TIMEOUTS(60000)
-
 
     def browser(self):
         return self._browser
